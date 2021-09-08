c92da2056c2803a318bee43765f3f1004f20df6c
pedrokorwa <korwapedro@gmail.com>
2021-09-08 20:46:39 +0700

    new

--- app/src/main/java/com/pedro/statemanagement/MainActivity.kt
+++ app/src/main/java/com/pedro/statemanagement/MainActivity.kt
@@ -3,11 +3,17 @@ package com.pedro.statemanagement
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pedro.statemanagement.ui.theme.StateManagementTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,23 +22,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             StateManagementTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+             Column {
+
+             }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StateMenajement() {
+
+   Row(modifier = Modifier.padding(10.dp) , verticalAlignment = Alignment.CenterVertically){
+
+       val checkBox
+   }
+
+
+
 }
 
 @Preview(showBackground = true , showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     StateManagementTheme {
-        Greeting("Android")
+        StateMenajement()
     }
 }
